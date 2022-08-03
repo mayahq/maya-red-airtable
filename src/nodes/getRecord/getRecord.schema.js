21ce01215bbd37d0155357da339e910556f9f904
vatsalsharma376 <vatsalsharma376@gmail.com>
2022-08-03 14:28:56 +0530

    added filters

--- src/nodes/getRecord/getRecord.schema.js
+++ src/nodes/getRecord/getRecord.schema.js
@@ -20,7 +20,28 @@ class GetRecord extends Node {
       AirtableConfig: new fields.ConfigNode({ type: AirtableConfig }),
       action: new fields.SelectFieldSet({
         fieldSets: {
-          getAllRecords: {},
+          getAllRecords: {
+            fields: new fields.Typed({
+              type: "str",
+              allowedTypes: ["str", "msg"],
+              defaultVal: "",
+            }),
+            filterByFormula: new fields.Typed({
+              type: "str",
+              allowedTypes: ["str", "msg"],
+              defaultVal: "",
+            }),
+            maxRecords: new fields.Typed({
+              type: "num",
+              allowedTypes: ["num", "msg"],
+              defaultVal: 100,
+            }),
+            sort: new fields.Typed({
+              type: "json",
+              allowedTypes: ["json", "msg"],
+              defaultVal: "{}",
+            }),
+          },
           getRecordById: {
             recordId: new fields.Typed({
               type: "msg",
@@ -51,7 +72,26 @@ class GetRecord extends Node {
       this.setStatus("PROGRESS", "Fetching records...");
 
       try {
-        const response = await base(tableName).select({}).all();
+        // check if vals.action.childValues.fields is a string then make it array of strings else leave it as it is
+        const fields =
+          typeof vals.action.childValues.fields === "string"
+            ? vals.action.childValues.fields.split(",")
+            : vals.action.childValues.fields;
+
+        let tableQuery = {
+          filterByFormula: vals.action.childValues.filterByFormula,
+          maxRecords: vals.action.childValues.maxRecords,
+        };
+        //check if vals.action.childValues.sort is an empty object
+
+        if (Object.keys(vals.action.childValues.sort).length !== 0) {
+          tableQuery.sort = vals.action.childValues.sort;
+        }
+        console.log(fields, "XXX", fields.length);
+        if (fields[0].length >= 1) {
+          tableQuery.fields = fields;
+        }
+        const response = await base(tableName).select(tableQuery).all();
         msg.records = response.map((record) => {
           return { id: record.id, ...record.fields };
         });
@@ -65,7 +105,9 @@ class GetRecord extends Node {
     } else if (vals.action.selected === "getRecordById") {
       this.setStatus("PROGRESS", "Fetching record...");
       try {
-        const response = await base(tableName).find(vals.action.childValues.recordId);
+        const response = await base(tableName).find(
+          vals.action.childValues.recordId
+        );
         msg.records = [{ id: response.id, ...response.fields }];
         this.setStatus("SUCCESS", "Record fetched");
       } catch (err) {
